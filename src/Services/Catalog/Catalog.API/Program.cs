5b15bffa9d6a36f813eb9b61cd266b4d36971989
sarah ramadan <sarah.ramadan.fahmy@gmail.com>
2025-07-05 00:22:53 +0300

    Enhance Kestrel configuration and logging settings
    Updated `Program.cs` to configure Kestrel and MediatR services, print environment name, and provide guidance on running the application with specific URLs. Modified `app.Run()` to support prioritized URL binding.
    
    Created `appsettings.Testing.json` for logging and Kestrel settings, ensuring the server listens on port 5010. Updated `appsettings.json` to include similar Kestrel configurations for consistent behavior across environments.

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,11 +1,28 @@
 
 using Catalog.API.Products.CreateProduct;
+using Microsoft.AspNetCore.Hosting;
 
 var builder = WebApplication.CreateBuilder(args);
+
 
 builder.Services.AddMediatR(cfg => {
     cfg.RegisterServicesFromAssembly(typeof(Program).Assembly/*Assembly.GetExecutingAssembly()*/);
 });
+
+//windows CMD: set DOTNET_ENVIRONMENT=Testing
+//windows CMD: dotnet run --no-launch-profile
+// app.UseUrls() - dotnet run --urls "http://..." - ASPNETCORE_URLS override by kestrel configuration 
+var kestrelConfig = builder.Configuration.GetSection("Kestrel");
+Console.WriteLine($"Environment : {builder.Environment.EnvironmentName}");
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.Configure(kestrelConfig);
+    //options.ListenAnyIP(5010); // HTTP on all interfaces
+});
+
+//Windows CMD: set ASPNETCORE_URLS=http://localhost:5000  if launchsettingis exist it will override this command
+//from CLI command  dotnet run --urls "http://localhost:6000" ->  priority 4
+//builder.WebHost.UseUrls("http://localhost:7000"); // priority 3
 
 var app = builder.Build();
 
@@ -13,4 +30,5 @@ var app = builder.Build();
 var group =app.MapGroup("/api");
 group.MapProductsAPIs();
 
-app.Run();
+//app.Urls.Add("http://localhost:8000");// priority 2
+app.Run(/*"http://localhost:9000"*/); // priority 1
