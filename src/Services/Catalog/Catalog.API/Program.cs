97ddaa83e03c49d276fad22880d8fce1c71f3a01
Sarah Ramadan Fahmy <sarah.fahmi@moj.gov.sa>
2025-07-13 22:59:44 +0300

    Renaming folder and projects

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -7,19 +7,20 @@ using SharedBlock.CQRS;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddMediatR(cfg => {
+builder.Services.AddMediatR(cfg =>
+{
     cfg.RegisterServicesFromAssembly(typeof(Program).Assembly/*Assembly.GetExecutingAssembly()*/);
 });
 
 // register CQRS services
 builder.Services.RegisterCQRSDependencies();
 builder.Services.AddScoped<ICommandHandler<CreateCategoryCommand>, CreateCategoryHandler>();
-builder.Services.AddScoped<IQueryHandler<GetCategoryById,Catalog.API.Models.Category>, GetCategoryHandler>();
+builder.Services.AddScoped<IQueryHandler<GetCategoryById, Catalog.API.Models.Category>, GetCategoryHandler>();
 
 var app = builder.Build();
 
 // register routing
-var group =app.MapGroup("/api").WithTags("Public");
+var group = app.MapGroup("/api").WithTags("Public");
 
 var productGroup = group.MapGroup("/products").WithTags("Product");
 productGroup.MapProductEndpoints();
